758684559df17acb67c2ed918124b160cc22c023
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2026-02-14 08:23:25 +0200

    dir-locals: add compile-command (commented-out)
    Add example of setting up compile-command; but comment out by default to
    avoid warnings loaded.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,15 @@
          (fill-column . 80)
          ;; (commment-fill-column . 80)
          (emacs-lisp-docstring-fill-column . 65)
-         (bug-reference-url-format . "https://github.com/djcb/mu/issues/%s")))
+         (bug-reference-url-format . "https://github.com/djcb/mu/issues/%s")
+         ;;   uncomment below (or put in .dir-locals-2.el)
+         ;;   to dtrt for M-x compile
+         ;;
+         ;; (eval . (setq compile-command
+         ;;               (format "make -C %s"
+         ;;                       (shell-quote-argument
+         ;;                        (locate-dominating-file "." "Makefile")))))
+         ))
  (c-mode . ((c-file-style . "linux")
             (indent-tabs-mode . t)
             (mode . bug-reference-prog)))
